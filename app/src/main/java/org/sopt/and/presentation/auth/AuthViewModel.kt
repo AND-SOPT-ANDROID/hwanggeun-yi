4ffeab45d9beba84b89704547328444d8e0999f6
yihwanggeun <wkwk0120@gmail.com>
2024-12-06 01:33:30 +0900

    #7 [ADD] API 통신 로직 추가

--- app/src/main/java/org/sopt/and/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/org/sopt/and/presentation/auth/AuthViewModel.kt
@@ -1,56 +1,74 @@
 package org.sopt.and.presentation.auth
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.sopt.and.R
+import org.sopt.and.core.data.dto.reqeust.CreateUserRequest
+import org.sopt.and.core.data.dto.reqeust.LoginRequest
+import org.sopt.and.network.ServicePool.authService
 import org.sopt.and.utils.KeyStorage
-import java.util.regex.Pattern.matches
+import retrofit2.HttpException
 
 class AuthViewModel(application: Application) : AndroidViewModel(application) {
 
     private val context = getApplication<Application>().applicationContext
 
-    private val _email = MutableLiveData("")
-    val email: LiveData<String> get() = _email
+    private val _username = MutableLiveData("")
+    val username: LiveData<String> get() = _username
 
     private val _password = MutableLiveData("")
     val password: LiveData<String> get() = _password
+
+    private val _hobby = MutableLiveData("")
+    val hobby: LiveData<String> get() = _hobby
 
     val signInSuccess = MutableLiveData(false)
     val signUpSuccess = MutableLiveData(false)
     val errorMessage = MutableLiveData("")
 
-    private val _savedEmail = MutableLiveData<String?>()
-    private val _savedPassword = MutableLiveData<String?>()
-
-    fun setEmail(email: String) {
-        _email.value = email
+    fun setUsername(username: String) {
+        _username.value = username
     }
 
     fun setPassword(password: String) {
         _password.value = password
     }
 
-    fun onSignInClick(onSignInSuccess: (String, String) -> Unit) {
-        viewModelScope.launch {
-            val emailValue = _email.value
-            val passwordValue = _password.value
-            Log.d("Tag", _savedEmail.value + _savedPassword.value + " : " + emailValue + passwordValue )
+    fun setHobby(hobby: String) {
+        _hobby.value = hobby
+    }
 
-            if (emailValue == _savedEmail.value && passwordValue == _savedPassword.value) {
+    fun onSignInClick(onSignInSuccess: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                val response = authService.login(
+                    LoginRequest(
+                        username = _username.value ?: "",
+                        password = _password.value ?: ""
+                    )
+                )
+                context.getSharedPreferences("auth", Context.MODE_PRIVATE).edit()
+                    .putString("token", response.result.token)
+                    .apply()
+
                 signInSuccess.value = true
                 errorMessage.value = context.getString(R.string.signin_success)
-                delay(1L)
-                onSignInSuccess(emailValue ?: "", passwordValue ?: "")
-            } else {
-                errorMessage.value = context.getString(R.string.signin_fail)
+                onSignInSuccess()
+            } catch (e: HttpException) {
+                when (e.code()) {
+                    400 -> errorMessage.value = context.getString(R.string.network_error_400)
+                    403 -> errorMessage.value = context.getString(R.string.network_error_403)
+                    else -> errorMessage.value = context.getString(R.string.signin_fail)
+                }
+                signInSuccess.value = false
+            } catch (e: Exception) {
+                errorMessage.value = context.getString(R.string.network_error)
                 signInSuccess.value = false
             }
         }
@@ -58,25 +76,46 @@ class AuthViewModel(application: Application) : AndroidViewModel(application) {
 
     fun onSignUpClick(onSignUpSuccess: () -> Unit) {
         viewModelScope.launch {
-            val emailValue = _email.value
-            val passwordValue = _password.value
+            if (!isValidInput()) {
+                errorMessage.value = context.getString(R.string.sign_up_error_8_over)
+                return@launch
+            }
 
-            if (isValidEmail(emailValue ?: "") && isValidPassword(passwordValue ?: "")) {
-                _savedEmail.value = emailValue
-                _savedPassword.value = passwordValue
+            if(!isValidPassword(_password.value ?: "")){
+                errorMessage.value = context.getString(R.string.sign_up_error_password_form)
+                return@launch
+            }
+            try {
+                val response = authService.signUp(
+                    CreateUserRequest(
+                        username = _username.value.orEmpty(),
+                        password = _password.value.orEmpty(),
+                        hobby = _hobby.value.orEmpty()
+                    )
+                )
+
                 signUpSuccess.value = true
                 errorMessage.value = context.getString(R.string.signup_success)
-                delay(1L)
                 onSignUpSuccess()
-            } else {
-                errorMessage.value = context.getString(R.string.sign_up_failure)
+            } catch (e: HttpException) {
+                when (e.code()) {
+                    400 -> errorMessage.value = context.getString(R.string.network_error_400)
+                    409 -> errorMessage.value = context.getString(R.string.network_error_409)
+                    else -> errorMessage.value = context.getString(R.string.signup_fail)
+                }
+                signUpSuccess.value = false
+            } catch (e: Exception) {
+                Log.e("SignUpError", "Exception occurred: ${e.message}", e)
+                errorMessage.value = context.getString(R.string.network_error)
                 signUpSuccess.value = false
             }
         }
     }
 
-    private fun isValidEmail(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    private fun isValidInput(): Boolean {
+        return (_username.value?.length ?: 0) <= 8 &&
+                (_password.value?.length ?: 0) <= 8 &&
+                (_hobby.value?.length ?: 0) <= 8
     }
 
     private fun isValidPassword(password: String): Boolean {
