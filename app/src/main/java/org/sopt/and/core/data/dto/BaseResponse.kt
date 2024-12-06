afb7302c065ea8ecf02e84437b7def909075356f
yihwanggeun <wkwk0120@gmail.com>
2024-12-06 19:22:40 +0900

    #10 [FIX] BaseResponse 구조 변경

--- app/src/main/java/org/sopt/and/core/data/dto/BaseResponse.kt
+++ app/src/main/java/org/sopt/and/core/data/dto/BaseResponse.kt
@@ -1,10 +1,10 @@
 package org.sopt.and.core.data.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse <T> (
-    val status: Int,
-    val message: String,
-    val data: T? = null
+    @SerialName("result")
+    val result: T
 )
